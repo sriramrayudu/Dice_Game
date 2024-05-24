cb25fa9cd7006b7292fe5ea306203a1897262161
sriramrayudu <109577033+sriramrayudu@users.noreply.github.com>
2024-05-24 15:17:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 =Math.floor( Math.random()*6)+1;
-var randomDiceImage="images/" +"dice"+randomNumber1+".png";
+var randomDiceImage="diceim/" +"dice"+randomNumber1+".png";
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomDiceImage);
 var randomNumber2 =Math.floor( Math.random()*6)+1;
-var randomDiceImage2="images/" +"dice"+randomNumber2+".png";
+var randomDiceImage2="diceim/" +"dice"+randomNumber2+".png";
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomDiceImage2);
 if (randomNumber1>randomNumber2){
